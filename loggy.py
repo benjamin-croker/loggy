83a481c819ace7216c1ca4bebcd86ece77686cea
Benjamin Croker <benjamin.croker@gmail.com>
2014-12-20 11:37:31 +1000

    change loggy output format

--- loggy.py
+++ loggy.py
@@ -3,8 +3,13 @@ import inspect
 
 def log(fn):
     def decorated(*args, **kwargs):
-        logging.debug("called {}\nargs: {}\nkeyword Args: {}".format(fn.__name__, args, kwargs))        
+        # get the names of all the args
+        arguments = inspect.getcallargs(fn, *args, **kwargs)
+        argstr = "\n".join(["{}: {}".format(a, arguments[a]) for a in arguments])
+
+        logging.debug("called {} with arguments:\n{}".format(fn.__name__, argstr))
         result = fn(*args, **kwargs)
         logging.debug("result: {}".format(result))
+
     return decorated
 
--- test_loggy.py
+++ test_loggy.py
@@ -4,8 +4,8 @@ import loggy
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 @loggy.log
-def foo(a, b):
-    return a+b
-
+def foo(a, b, c=4, d=5, e=6):
+    return a+b+c+d+e
 
 foo(1, 3)
+foo(1, 3, 5, e=90)
